680cbea7bcb9f3eedfdb69b32518ccb8d16d0973
Michał Miasojedow <michal@miasojedow.com>
2020-02-15 18:21:17 +0100

    models added

--- OddamWDobreRece/models.py
+++ OddamWDobreRece/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+
+TYPES = (
+    (1, 'Fundacja'),
+    (2, 'Organizacja pozarządowa'),
+    (3, 'Zbiórka lokalna'),
+)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    type = models.IntegerField(choices=TYPES, default=1)
+    categories = models.ManyToManyField('Category')
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField('Category')
+    institution = models.ForeignKey('Institution', on_delete=models.CASCADE)
+    address = models.CharField(max_length=128)
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=128)
+    zip_code = models.CharField(max_length=6)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, null=True, default=None, on_delete=models.CASCADE)
