982a7a0e18ed59d45da5e9d60c30420b8c775ddf
Michał Miasojedow <michal@miasojedow.com>
2020-02-15 23:49:21 +0100

    dynamic statistics

--- OddamWDobreRece/admin.py
+++ OddamWDobreRece/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from OddamWDobreRece.models import *
+
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ['name']
+
+
+class InstitutionAdmin(admin.ModelAdmin):
+    fields = ['name', 'description', 'type', 'categories']
+
+class DonationAdmin(admin.ModelAdmin):
+    fields = ['quantity', 'categories', 'institution', 'address', 'phone_number', 'city', 'zip_code', 'pick_up_date', 'pick_up_time', 'pick_up_comment', 'user']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Institution, InstitutionAdmin)
+admin.site.register(Donation, DonationAdmin)
+
--- OddamWDobreRece/views.py
+++ OddamWDobreRece/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from OddamWDobreRece.models import *
 
 
 # Create your views here.
@@ -7,7 +8,15 @@ from django.views import View
 
 class LandingPageView(View):
     def get(self, request):
-        return render(request, 'base.html')
+        sacks_counter = 0
+        supported_institutions = []
+        donations = Donation.objects.all()
+        for donation in donations:
+            sacks_counter += donation.quantity
+            if donation.institution.name not in supported_institutions:
+                supported_institutions.append(donation.institution.name)
+        supported_institutions_counter = len(supported_institutions)
+        return render(request, 'base.html', {'sacks': sacks_counter, 'institutions': supported_institutions_counter})
 
 
 class AddDonationView(View):
