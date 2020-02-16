a556e42d517cadff13c52a862349a928d9a6f97d
Michał Miasojedow <michal@miasojedow.com>
2020-02-16 09:35:55 +0100

    changes for admin interface

--- OddamWDobreRece/models.py
+++ OddamWDobreRece/models.py
@@ -1,12 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 # Create your models here.
-
-class Category(models.Model):
-    name = models.CharField(max_length=128)
-
 
 TYPES = (
     (1, 'Fundacja'),
@@ -15,11 +10,21 @@ TYPES = (
 )
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
 class Institution(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField()
     type = models.IntegerField(choices=TYPES, default=1)
     categories = models.ManyToManyField('Category')
+
+    def __str__(self):
+        return self.name
 
 
 class Donation(models.Model):
@@ -33,4 +38,10 @@ class Donation(models.Model):
     pick_up_date = models.DateField()
     pick_up_time = models.TimeField()
     pick_up_comment = models.TextField()
-    user = models.ForeignKey(User, null=True, default=None, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, default=None, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        if not self.user:
+            return 'Dla %s, worki: %s' % (self.institution, self.quantity)
+        else:
+            return '%s dla %s, worki: %s' % (self.user.username, self.institution, self.quantity)
