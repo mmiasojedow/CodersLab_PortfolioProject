9ec9ea8364d6b0c1f3ebd5917a1c222c291c4bc3
Michał Miasojedow <michal@miasojedow.com>
2020-02-16 12:34:54 +0100

    institutions dynamic loading

--- OddamWDobreRece/views.py
+++ OddamWDobreRece/views.py
@@ -16,7 +16,13 @@ class LandingPageView(View):
             if donation.institution.name not in supported_institutions:
                 supported_institutions.append(donation.institution.name)
         supported_institutions_counter = len(supported_institutions)
-        return render(request, 'base.html', {'sacks': sacks_counter, 'institutions': supported_institutions_counter})
+
+        foundations = Institution.objects.filter(type=1)
+        organizations = Institution.objects.filter(type=2)
+        locals = Institution.objects.filter(type=3)
+        return render(request, 'base.html',
+                      {'sacks': sacks_counter, 'institutions': supported_institutions_counter,
+                       'foundations': foundations, 'organizations': organizations, 'locals': locals})
 
 
 class AddDonationView(View):
