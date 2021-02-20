385bd2dc7343d74d3de2e8d767e60b53fc12b0cb
mmiasojedow <michal@miasojedow.com>
2021-02-20 17:51:34 +0100

    vs commit

--- donations/admin.py
+++ donations/admin.py
@@ -4,4 +4,10 @@ from donations.models import Category, Donation, Institution
 
 admin.site.register(Category)
 admin.site.register(Institution)
-admin.site.register(Donation)
+
+
+class DonationAdmin(admin.ModelAdmin):
+    list_display = ['user', 'quantity', 'institution']
+
+
+admin.site.register(Donation, DonationAdmin)
